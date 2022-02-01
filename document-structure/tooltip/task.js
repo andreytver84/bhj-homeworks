e0b5e1ad63734579e16d63ace350bc2cf77135dd
andreytver84 <uvikmedium@mail.ru>
2022-02-01 11:13:28 +0300

    6th task html structure I hope is done finaly

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,17 +1,23 @@
 window.addEventListener('load', () => {
-
-    //document.addEventListener('scroll', () => hideTips());
-
+    
     const toolTips = document.querySelectorAll('.has-tooltip');
 
     toolTips[0].insertAdjacentHTML("afterEnd", `<div class="tooltip"></div>`);
 
     const blockTip = document.querySelector('.tooltip');
 
+    let ind;
+
     function getTips(link) {
         const tip = link.getAttribute('title');
-        blockTip.innerText = `${tip}`;
-        console.log(blockTip);
+        const text = blockTip.innerText;
+        if (tip == text) {           
+            blockTip.classList.toggle('tooltip_active');
+            ind = 0;
+        } else {
+            blockTip.innerText = `${tip}`;       
+            ind = 1 ;           
+        }        
     }
 
     function removeActive(block) {
@@ -19,19 +25,18 @@ window.addEventListener('load', () => {
     }
 
     function showTips(event) {
+        
         event.preventDefault();
         const target = event.target;
+
         getTips(target);
-        console.dir(target);
-        if (!blockTip.classList.contains('tooltip_active')) {
-            blockTip.classList.add('tooltip_active');
-        }
-
-
-        blockTip.style.left = `${target.offsetLeft}px`;
-        blockTip.style.top = `${target.getBoundingClientRect().bottom}px`;
-
-        setTimeout(removeActive, 2000, blockTip);
+        
+        if (ind !== 0) {
+            blockTip.classList.add('tooltip_active');                               
+        }        
+        
+        blockTip.style.left = `${target.getBoundingClientRect().left}px`;
+        blockTip.style.top = `${target.getBoundingClientRect().bottom}px`;       
     }
 
     toolTips.forEach(item => {
